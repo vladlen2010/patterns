39bcdf40a4e6533dfdde94f85ab9b6edf062a431
Vladlen <v.ivanov@midhub.io>
2021-09-04 22:28:08 +0300

    add builder

---
+++ src/patterns/BuilderExample.kt
@@ -0,0 +1,41 @@
+package patterns
+
+data class Mail(
+    val to: String,
+    var title: String = "",
+    var _message: String = "",
+    val cc: List<String> = listOf(),
+    val bcc: List<String> = listOf(),
+    val attachments: List<java.io.File> = listOf()
+) {
+
+    fun message(message: String) = apply {
+        _message = message
+    }
+}
+
+/**
+ * builder design pattern
+ */
+class MailBuilder(to: String) {
+    private var mail: Mail = Mail(to)
+    fun title(title: String): MailBuilder {
+        mail.title = title
+        return this
+    }
+
+    /*
+    ...
+     */
+
+    fun build(): Mail {
+        return mail
+    }
+}
+
+fun main() {
+    val mail = Mail("hello").message("How are you?")
+    val mailBuilder = MailBuilder("hello").title("Summer letter").build()
+    println(mail._message)
+    println(mailBuilder.title)
+}
